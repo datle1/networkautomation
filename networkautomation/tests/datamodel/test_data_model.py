26764d9189d3e33e4c0c8ba0b7d9004a9df32e8a
Dat Le <datlq3@viettel.com.vn>
2021-06-21 11:22:08 +0700

    Add test for fail validation

--- networkautomation/tests/datamodel/test_data_model.py
+++ networkautomation/tests/datamodel/test_data_model.py
@@ -5,8 +5,15 @@ from networkautomation import data_model
 
 class TestDataModel(TestCase):
     def test_validate_vlan(self):
-        data = {'vlan-config': {'vlan-id': 4094, 'name': 'test'}}
+        data = {'vlan_config': {'vlan_id': 4094, 'name': 'test'}}
         data_model.DataModel.validate_data(data)
+
+    def test_validate_vlan(self):
+        data = {'vlan_config': {'vlan_id': 4095, 'name': 'test'}}
+        with self.assertRaises(Exception) as context:
+            data_model.DataModel.validate_data(data)
+        self.assertTrue('Error in pyangbind datamodel' in str(
+            context.exception))
 
     def test_validate_interface(self):
         data = {'interfaces': {'interface': {'Eth0': {'name':
